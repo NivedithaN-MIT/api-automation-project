92bf64be7219362151301b04ae4d2fbd3213edf6
Niveditha Namuduri <niveditha.n@mastechdigital.com>
2025-06-16 21:53:31 +0530

    Fix CI/CD setup, add deploy.yml

--- tests/test_openweathermap.py
+++ tests/test_openweathermap.py
@@ -1,20 +1,17 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from apis.openweathermap import OpenWeatherAPI
 
-# Load environment variables from .env
 load_dotenv()
-
 
 def test_weather_api():
     api_key = os.getenv("OPENWEATHER_API_KEY")
-    assert api_key is not None, "OPENWEATHER_API_KEY not set in environment"
+    assert api_key, "OPENWEATHER_API_KEY not set in environment"
 
     weather_api = OpenWeatherAPI(api_key)
     response = weather_api.get_weather("London")
 
-    assert response.status_code == 200, "API call failed"
-
+    assert response.status_code == 200
     data = response.json()
-    assert "weather" in data, "'weather' key missing in response"
-    assert "main" in data, "'main' key missing in response"
+    assert "weather" in data
+    assert "main" in data
